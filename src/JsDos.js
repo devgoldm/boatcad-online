27e16cdb1685de5bcd6fdacf374f221b07cbd6fc
Michael Goldm <michael@goldm.dev>
2021-02-07 11:13:23 +0000

    Display key press event for debugging Update BL3D.zip with help doc and removed address

--- src/JsDos.js
+++ src/JsDos.js
@@ -1,11 +1,17 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
+import Keyboard from 'react-simple-keyboard';
 import zipFile from "./BL3D.zip";
+import keycode from 'keycode';
+import 'react-simple-keyboard/build/css/index.css';
 require("js-dos");
 const Dos = window.Dos;
+const DosController = window.DosController;
 
 
 const JsDos = () => {
     const ref = useRef(null);
+    const [windowCi, setWindowCi] = useState(null);
+    const [text, setText] = useState("Waiting for keyboard input...");
 
     useEffect(() => {
         if (ref !== null) {
@@ -16,6 +22,9 @@ const JsDos = () => {
             }).then((runtime) => {
                 return runtime.fs.extract(zipFile).then(() => {
                     return runtime.main(["-c", "BL3D.EXE"]);
+                }).then(ci => {
+                  setWindowCi(ci); 
+                  return ci;
                 });
             });
 
@@ -27,8 +36,19 @@ const JsDos = () => {
         }
     }, [ref]);
 
+    useEffect(() => {
+      document.addEventListener("keydown", (event) => {setText(`KeyboardEvent: key='${event.key}' | code='${event.code}'`); pressKey(event.code)});
+    }, []);
+
+    const pressKey = (code) => {
+      windowCi?.simulateKeyEvent(code, true);
+      windowCi?.simulateKeyEvent(code, false);
+      console.log("Key code pressed = ", code);
+    };
+
     return (
     <div>
+      <p style={{color: "white"}}>{text}</p>
       <canvas ref={ref} />
     </div> 
     );
