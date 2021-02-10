a42a68f51cc85b84b0b2fbd71cf3a46aea9f69fc
Michael Goldm <michael@goldm.dev>
2021-02-10 00:07:31 +0000

    Change service worker to try updating critical data from network first

--- src/service-worker.js
+++ src/service-worker.js
@@ -11,7 +11,7 @@ import { clientsClaim } from 'workbox-core';
 import { ExpirationPlugin } from 'workbox-expiration';
 import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
-import { StaleWhileRevalidate } from 'workbox-strategies';
+import { StaleWhileRevalidate, NetworkFirst } from 'workbox-strategies';
 
 clientsClaim();
 
@@ -48,14 +48,28 @@ registerRoute(
 
 // An example runtime caching route for requests that aren't handled by the
 // precache, in this case same-origin .png requests like those from in public/
-registerRoute(
+StaleWhileRevalidate(
   // Add in any other file extensions or routing criteria as needed.
   ({ url }) => url.origin === self.location.origin && 
-  (url.pathname.endsWith('.png') || url.pathname.endsWith('.js') || 
-   url.pathname.endsWith('.zip') || url.pathname.endsWith('.css') || 
-   url.pathname.endsWith('.html') || url.pathname.endsWith('.json') ), // Customize this strategy as needed, e.g., by changing to CacheFirst.
+  (url.pathname.endsWith('.png')), // Customize this strategy as needed, e.g., by changing to CacheFirst.
   new StaleWhileRevalidate({
     cacheName: 'images',
+    plugins: [
+      // Ensure that once this runtime cache reaches a maximum size the
+      // least-recently used images are removed.
+      new ExpirationPlugin({ maxEntries: 100 }),
+    ],
+  })
+);
+
+NetworkFirst(
+  // Add in any other file extensions or routing criteria as needed.
+  ({ url }) => url.origin === self.location.origin && 
+  (url.pathname.endsWith('.js') || url.pathname.endsWith('.zip') || 
+   url.pathname.endsWith('.css') || url.pathname.endsWith('.html') ||
+   url.pathname.endsWith('.json') ),
+  new NetworkFirst({
+    cacheName: 'data',
     plugins: [
       // Ensure that once this runtime cache reaches a maximum size the
       // least-recently used images are removed.
